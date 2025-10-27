a936ab1fe183537e1fad2307884a721efd8a30ff
kchadu868-hub <kchadu868@gmail.com>
2025-10-27 12:50:25 +0530

    Update bot token and channel ID in m.py

--- m.py
+++ m.py
@@ -12,9 +12,9 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from threading import Thread
 
 loop = asyncio.get_event_loop()
-TOKEN = '8237142456:AAE0W-p38QV4blmXhOBBIFPGacXl4WvdH5Y'
+TOKEN = '7412729843:AAFPGH38YDtiQsHk18Q8aR-oYcI2by1Lcjk'
 MONGO_URI = 'mongodb+srv://ihatemosquitos9:JvOK4gNs0SH5SVw9@cluster0.1pd5kt5.mongodb.net/?appName=Cluster0'
-CHANNEL_ID = -1002416240231
+CHANNEL_ID = -1002748918836
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
